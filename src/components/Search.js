79ad67eee6de2f567a408cfa79db0ae0f91de725
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-24 15:31:20 +0530

    all

---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+const Search = () => {
+    let [item, setItem] = useState('');
+    let [all, setAll] = useState(fruits);
+
+    useEffect(()=>{
+        let update = fruits.filter(val=>(
+            val.includes(item)
+        ))
+        setAll(update)
+    }, [item])
+
+  return (
+    <div>
+        <h1>Search items</h1>
+        <input type="text" onChange={(e)=>{setItem(e.target.value)}}></input>
+        <ul>
+            {
+                all&& all.map(val=>(
+                    <li>{val}</li>
+                ))
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default Search
